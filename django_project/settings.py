b44ba52f9ab25fcd5a3652e0583d13f5654b0b10
pauloccardona <50184516+pauloccardona@users.noreply.github.com>
2023-11-30 16:33:47 +0000

    pruebas en settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -69,7 +69,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR,'django_project/templates').replace('\\','/'),
+            os.path.join(BASE_DIR,'/django_project/templates'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
