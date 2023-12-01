4d6c21816ebb4387d695b9c14268974bb7e3a0ac
pauloccardona <50184516+pauloccardona@users.noreply.github.com>
2023-12-01 10:56:31 +0000

    prueba 1 sin la app core

--- django_project/urls.py
+++ django_project/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django_project.core import views
+from . import views
 
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls)
 ]
