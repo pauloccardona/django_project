542f6712eda21661a96477cac2f36fd10b607019
pauloccardona <50184516+pauloccardona@users.noreply.github.com>
2023-12-03 16:33:29 +0000

    test3 no usando la app

--- django_project/urls.py
+++ django_project/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core import views
-#from . import views
+#from core import views
+from . import views
 
 
 
