0dab4b4f366fddda2c2be13ff9fb1fb4eeeb8832
pauloccardona <50184516+pauloccardona@users.noreply.github.com>
2023-12-06 21:11:44 +0000

    Prueba7.1 organizar ruta directorio templates

--- django_project/settings.py
+++ django_project/settings.py
@@ -68,7 +68,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR,'templates'),
+            BASE_DIR / 'django_project/templates',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
