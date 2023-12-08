ce769d00b622a75e07eb89066ad384f54fa95ab6
pauloccardona <50184516+pauloccardona@users.noreply.github.com>
2023-12-08 05:27:21 +0000

    tutorial4 including views file

--- core/views.py
+++ core/views.py
@@ -1,4 +1,22 @@
-from django.shortcuts import render
+ffrom django.shortcuts import render,redirect
+from django.contrib.auth.forms import User
+from django.contrib.auth.forms import UserCreationForm
 
+# Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    count = User.objects.count()
+    return render(request,"home.html",{
+        'count': count
+    })
+
+def signup(request):
+    if request.method == "POST":
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form=UserCreationForm()
+    return render(request,"registration/signup.html",{
+        'form': form
+    })
