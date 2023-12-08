9bf8c74665e809edc5179c6b6a8974ba7e44fb88
pauloccardona <50184516+pauloccardona@users.noreply.github.com>
2023-12-08 04:47:23 +0000

    Tutorial part4: login - logout

--- django_project/urls.py
+++ django_project/urls.py
@@ -21,6 +21,8 @@ from core import views
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('admin/', admin.site.urls)
+    path("", views.home, name="home"),
+    path("signup/", views.signup, name="signup"),
+    path("accounts/", include('django.contrib.auth.urls')),
+    path("admin/", admin.site.urls),
 ]
