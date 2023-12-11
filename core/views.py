fa847372f6b891cf9703b1a8ace66490ccfa6cca
pauloccardona <50184516+pauloccardona@users.noreply.github.com>
2023-12-11 13:42:27 +0000

    Tutorial7: Protected View correction

--- core/views.py
+++ core/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import User
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
-from django.contrib.auth.mixins import loginRequiredMixins
+from django.contrib.auth.mixins import loginRequiredMixin
 
 # Create your views here.
 def home(request):
