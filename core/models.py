f4237042d319f8c05b8dfe042117bf46c08b2552
pauloccardona <50184516+pauloccardona@users.noreply.github.com>
2024-01-02 23:45:43 +0000

    modelos basicos

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Transaction(models.Model):
+    code = models.CharField(max_length=10)
+
+class Plate(models.Model):
+    name = models.CharField(max_length=32)
+    transactions = models.ManyToManyField(Transaction)
+
+class Extra(models.Model):
+    name = models.CharField(max_length=16)
+    plates = models.ManyToManyField(Plate)
